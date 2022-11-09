286b9fb15e0cb189ed3a9652fd4e03d81baa2e7b
unknown <inggrid>
2022-11-09 11:09:40 +0700

    Initial commit

---
+++ app/src/main/java/com/smktelkommlg/kalkulator/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.smktelkommlg.kalkulator
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+    }
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_calculate) {
+
+            var inputLength: String =
+                edtLength.getText().toString().trim();
+
+            var inputWidth: String =
+                edtWidth.getText().toString().trim();
+
+            var inputHeight: String =
+                edtHeight.getText().toString().trim();
+
+            var isEmptyFields = false
+            var isInvalidDouble = false
+
+            if (TextUtils.isEmpty(inputLength)) {
+                isEmptyFields = true
+                edtLength.error = "Field ini tidak boleh kosong"
+            }
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true
+                edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true
+                edtHeight.error = "Field ini tidak boleh kosong"
+
+            }
+            val length = convertToDouble(inputLength);
+            val width = convertToDouble(inputWidth);
+            val height = convertToDouble(inputHeight);
+
+            if (length == null) {
+                isInvalidDouble = true
+                edtLength.error = "nilai tidak valid"
+            }
+            if (width == null) {
+                isInvalidDouble = true
+                edtWidth.error = "nilai tidak valid"
+            }
+
+            if (height == null) {
+                isInvalidDouble = true
+                edtHeight.error = "nilai tidak valid"
+            }
+
+            if (!isEmptyFields && !isInvalidDouble) {
+                val volume = height!!.toDouble() * length!!.toDouble() * width!!.toDouble()
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+    private fun convertToDouble(str: String):Double?{
+        return try {
+            str.toDouble()
+        }catch (e : NumberFormatException){
+            null
+        }
+    }
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+        }
+}
---
+++ app/src/main/java/com/smktelkommlg/kalkulator/biodata.kt
@@ -0,0 +1,11 @@
+package com.smktelkommlg.kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class biodata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata)
+    }
+}
